5338bae5dc1d42d2043697755204b0a7ea0c1364
drf0rk <wolfparade@hotmail.com>
2025-06-07 23:43:42 +1200

    Update _loras-data.py
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

--- scripts/_loras-data.py
+++ scripts/_loras-data.py
@@ -1,14 +1,23 @@
 ## LORA
 
-lora_list = {
-    "1. nudify-xl": [
-        {'url': "https://civitai.com/models/9025?modelVersionId=177674", 'name': "nudify_xl_lite.safetensors"}
-    ],
-    "2. Example LoRA 2 (another option)": [
-        {'url': "https://civitai.com/models/10364?modelVersionId=12873", 'name': "innievag.safetensors"}
-    ]
-    # Add your own LoRA entries here following the format:
-    # "NUMBER. Your LoRA Name": [
-    #     {'url': "https://civitai.com/api/download/models/YOUR_ACTUAL_LORA_ID", 'name': "your_lora_filename.safetensors"}
-    # ]
+lora_data = {
+    "sd15_loras": {
+        "1. Example SD1.5 LoRA A": [
+            {'url': "https://civitai.com/api/download/models/YOUR_SD15_LORA_ID_A", 'name': "sd15_lora_a.safetensors"}
+        ],
+        "2. Example SD1.5 LoRA B": [
+            {'url': "https://civitai.com/api/download/models/YOUR_SD15_LORA_ID_B", 'name': "sd15_lora_b.safetensors"}
+        ]
+        # Add your own SD 1.5 LoRA entries here
+    },
+    "sdxl_loras": {
+        "1. nudify-xl": [
+            {'url': "https://civitai.com/models/9025?modelVersionId=177674", 'name': "nudify_xl_lite.safetensors"}
+        ],
+        "2. Example LoRA 2 (another option)": [
+            {'url': "https://civitai.com/models/10364?modelVersionId=12873", 'name': "innievag.safetensors"}
+        ]
+        # Add your own SDXL LoRA entries here
+    }
 }
+
