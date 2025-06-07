9c248c88271935400cf57a651bf28b18e9b6f046
drf0rk <wolfparade@hotmail.com>
2025-06-07 23:11:07 +1200

    Update _loras-data.py
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

--- scripts/_loras-data.py
+++ scripts/_loras-data.py
@@ -1,11 +1,11 @@
 ## LORA
 
 lora_list = {
-    "1. Example LoRA 1": [
-        {'url': "https://civitai.com/api/download/models/YOUR_LORA_ID_1", 'name': "example_lora_1.safetensors"}
+    "1. nudify-xl": [
+        {'url': "https://civitai.com/models/9025/nudify-xl-better-bodies?modelVersionId=177674", 'name': "nudify_xl_lite.safetensors"}
     ],
     "2. Example LoRA 2 (another option)": [
-        {'url': "https://civitai.com/api/download/models/YOUR_LORA_ID_2", 'name': "example_lora_2.safetensors"}
+        {'url': "https://civitai.com/api/download/models/YOUR_LORA_ID_2", 'name': "nudify_xl_lite.safetensors"}
     ]
     # Add your own LoRA entries here following the format:
     # "NUMBER. Your LoRA Name": [
