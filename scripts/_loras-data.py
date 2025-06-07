80b535ab8ab4520e1fa0d8cdf973ddd1daa9ef28
drf0rk <wolfparade@hotmail.com>
2025-06-07 22:46:09 +1200

    Create _loras-data.py
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

---
+++ scripts/_loras-data.py
@@ -0,0 +1,14 @@
+## LORA
+
+lora_list = {
+    "1. Example LoRA 1": [
+        {'url': "https://civitai.com/api/download/models/YOUR_LORA_ID_1", 'name': "example_lora_1.safetensors"}
+    ],
+    "2. Example LoRA 2 (another option)": [
+        {'url': "https://civitai.com/api/download/models/YOUR_LORA_ID_2", 'name': "example_lora_2.safetensors"}
+    ],
+    # Add your own LoRA entries here following the format:
+    # "NUMBER. Your LoRA Name": [
+    #     {'url': "https://civitai.com/api/download/models/YOUR_ACTUAL_LORA_ID", 'name': "your_lora_filename.safetensors"}
+    # ]
+}
