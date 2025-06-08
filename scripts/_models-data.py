02251f879299655dfa5b79e2977c79e7c1265978
drf0rk <wolfparade@hotmail.com>
2025-06-08 21:29:00 +1200

    Update _models-data.py
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

--- scripts/_models-data.py
+++ scripts/_models-data.py
@@ -1,40 +1,40 @@
 ## MODEL
 
 model_list = {
-    "1. AcornMoarMindBreak [SD1.5]": [
+    "1. AcornMoarMindBreak by Remphanstar [SD1.5]": [
         {'url': "https://huggingface.co/Remphanstar/Rojos/resolve/main/SD.15-AcornMoarMindBreak.safetensors", 'name': "SD.15-AcornMoarMindBreak.safetensors"}
     ],
-    "2. Lustify (Inpainting) [SD1.5]": [
+    "2. Lustify Inpainting by RandomGulag [SD1.5]": [
         {'url': "https://huggingface.co/RandomGulag/lustifySDXLNSFW_oltINPAINTING/resolve/main/lustifySDXLNSFW_oltINPAINTING.safetensors", 'name': "lustifySDXLNSFW_oltINPAINTING.safetensors"}
     ],
-    "3. LazyMix Real Amateur [SD1.5]": [
-        {'url': "https://civitai.com/models/10961/lazymix-real-amateur-nudes?modelVersionId=300972", 'name': "lazymix-real-amateur-nudes.safetensors"}
+    "3. LazyMix Real Amateur Nudes [SD1.5]": [
+        {'url': "https://civitai.com/models/10961/lazymix-real-amateur-nudes?modelVersionId=300972", 'name': "lazymixRealAmateur_v10.safetensors"}
     ],
-    "4. Inpainting Model 1 [SD1.5]": [
-        {'url': "https://civitai.com/api/download/models/188884?type=Model&format=SafeTensor&size=full&fp=fp16", 'name': "inpainting_model_1.safetensors"}
+    "4. Unstable Ink Inpainting [SD1.5]": [
+        {'url': "https://civitai.com/api/download/models/188884?type=Model&format=SafeTensor&size=full&fp=fp16", 'name': "unstableInk_v10Inpainting.safetensors"}
     ],
-    "5. Pornmaster Pro (Inpainting) [SD1.5]": [
-        {'url': "https://civitai.com/models/1031352/pornmaster-pro-full-v4-inpainting", 'name': "pornmaster-pro-v4-inpainting.safetensors"}
+    "5. Pornmaster Pro v4 Inpainting [SD1.5]": [
+        {'url': "https://civitai.com/models/1031352/pornmaster-pro-full-v4-inpainting", 'name': "pornmasterProFull_v4Inpainting.safetensors"}
     ],
-    "6. Extra Model 1 [SD1.5]": [
-        {'url': "https://civitai.com/api/download/models/179318?type=Model&format=SafeTensor&size=full&fp=fp16", 'name': "extra_model_1.safetensors"}
+    "6. Real X 2.2 [SD1.5]": [
+        {'url': "https://civitai.com/api/download/models/179318?type=Model&format=SafeTensor&size=full&fp=fp16", 'name': "realx22_original.safetensors"}
     ],
-    "7. D5K6.0 [SD1.5]": [
+    "7. D5K6.0 by Remphanstar [SD1.5]": [
         {'url': "https://huggingface.co/Remphanstar/Rojos/resolve/main/D5K6.0.safetensors?download=true", 'name': "D5K6.0.safetensors"}
     ],
-    "8. Extra Inpainting Model [SD1.5]": [
-        {'url': "https://civitai.com/api/download/models/95864?type=Model&format=SafeTensor&size=pruned&fp=fp16", 'name': "extra_inpainting_model.safetensors"}
+    "8. Deliberate Inpainting v2 [SD1.5]": [
+        {'url': "https://civitai.com/api/download/models/95864?type=Model&format=SafeTensor&size=pruned&fp=fp16", 'name': "deliberate_v2Inpainting.safetensors"}
     ]
 }
 
 ## VAE
 
 vae_list = {
-    "1. SD1.5 VAE 1": [
-        {'url': "https://civitai.com/api/download/models/88156?type=Model&format=SafeTensor", 'name': "sd15_vae_1.safetensors"}
+    "1. VAE-ft-mse-840000-ema-pruned": [
+        {'url': "https://civitai.com/api/download/models/88156?type=Model&format=SafeTensor", 'name': "vae-ft-mse-840000-ema-pruned.safetensors"}
     ],
-    "2. SD1.5 VAE 2": [
-        {'url': "https://civitai.com/api/download/models/311162?type=Model&format=SafeTensor", 'name': "sd15_vae_2.safetensors"}
+    "2. vae-ft-ema-560000-pruned": [
+        {'url': "https://civitai.com/api/download/models/311162?type=Model&format=SafeTensor", 'name': "vae-ft-ema-560000-pruned.safetensors"}
     ]
 }
 
@@ -52,5 +52,47 @@ controlnet_list = {
     "3. Depth": [
         {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth_fp16.safetensors"},
         {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11f1p_sd15_depth_fp16.yaml"}
+    ],
+    "4. Lineart": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_lineart_fp16.yaml"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15s2_lineart_anime_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15s2_lineart_anime_fp16.yaml"}
+    ],
+    "5. ip2p": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11e_sd15_ip2p_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11e_sd15_ip2p_fp16.yaml"}
+    ],
+    "6. Shuffle": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11e_sd15_shuffle_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11e_sd15_shuffle_fp16.yaml"}
+    ],
+    "7. Inpaint": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_inpaint_fp16.yaml"}
+    ],
+    "8. MLSD": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_mlsd_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_mlsd_fp16.yaml"}
+    ],
+    "9. Normalbae": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_normalbae_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_normalbae_fp16.yaml"}
+    ],
+    "10. Scribble": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_scribble_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_scribble_fp16.yaml"}
+    ],
+    "11. Seg": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_seg_fp16.yaml"}
+    ],
+    "12. Softedge": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_softedge_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_softedge_fp16.yaml"}
+    ],
+    "13. Tile": [
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors"},
+        {'url': "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11f1e_sd15_tile_fp16.yaml"}
     ]
 }
