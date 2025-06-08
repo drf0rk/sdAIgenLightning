342e153bb68fb51fb766f4604e8fc67e7d682f5b
drf0rk <wolfparade@hotmail.com>
2025-06-09 01:59:31 +1200

    Update _xl-models-data.py
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

--- scripts/_xl-models-data.py
+++ scripts/_xl-models-data.py
@@ -16,8 +16,72 @@ sdxl_vae_data = {
     "SDXL VAE - SDXL-VAE": {"url": "https://civitai.com/api/download/models/333245", "name": "sdxlVAE_sdxlVAE.safetensors"},
 }
 
-# SDXL CONTROLNETS
-sdxl_controlnet_list = {
-    "1. Kohya Controllite XL Blur": [{"url": "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_blur.safetensors"}],
+## CONTROLNET
+
+controlnet_list = {
+    "1. Kohya Controllite XL Blur": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_blur.safetensors"},
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_blur_anime.safetensors"}
+    ],
+    "2. Kohya Controllite XL Canny": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_canny.safetensors"},
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_canny_anime.safetensors"}
+    ],
+    "3. Kohya Controllite XL Depth": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_depth.safetensors"},
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_depth_anime.safetensors"}
+    ],
+    "4. Kohya Controllite XL Openpose Anime": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_openpose_anime.safetensors"},
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_openpose_anime_v2.safetensors"}
+    ],
+    "5. Kohya Controllite XL Scribble Anime": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_scribble_anime.safetensors"}
+    ],
+    "6. T2I Adapter XL Canny": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_xl_canny.safetensors"}
+    ],
+    "7. T2I Adapter XL Openpose": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_xl_openpose.safetensors"}
+    ],
+    "8. T2I Adapter XL Sketch": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_xl_sketch.safetensors"}
+    ],
+    "9. T2I Adapter Diffusers XL Canny": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_canny.safetensors"}
+    ],
+    "10. T2I Adapter Diffusers XL Depth Midas": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_depth_midas.safetensors"}
+    ],
+    "11. T2I Adapter Diffusers XL Depth Zoe": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_depth_zoe.safetensors"}
+    ],
+    "12. T2I Adapter Diffusers XL Lineart": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_lineart.safetensors"}
+    ],
+    "13. T2I Adapter Diffusers XL Openpose": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_openpose.safetensors"}
+    ],
+    "14. T2I Adapter Diffusers XL Sketch": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_diffusers_xl_sketch.safetensors"}
+    ],
+    "15. IP Adapter SDXL": [
+        {'url': "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl.safetensors"}
+    ],
+    "16. IP Adapter SDXL VIT-H": [
+        {'url': "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl_vit-h.safetensors"}
+    ],
+    "17. Diffusers XL Canny Mid": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_mid.safetensors"}
+    ],
+    "18. Diffusers XL Depth Mid": [
+        {'url': "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_depth_mid.safetensors"}
+    ],
+    "19. Controlnet Union SDXL 1.0": [
+        {'url': "https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model.safetensors", 'name': "controlnet-union-sdxl-1.0.safetensors"}
+    ],
+    "20. Controlnet Union SDXL Pro Max": [
+        {'url': "https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model_promax.safetensors", 'name': "controlnet-union-sdxl-promax.safetensors"}
+    ]
 }
 
