23fbd3aff6d4793fc5808212894eea055da6ab30
drf0rk <wolfparade@hotmail.com>
2025-06-09 01:35:45 +1200

    Add files via upload
    Signed-off-by: drf0rk <wolfparade@hotmail.com>

---
+++ scripts/_models-data.py
@@ -0,0 +1,25 @@
+# SD 1.5 MODELS
+sd15_model_data = {
+    "D5K6.0": {"url": "https://huggingface.co/Remphanstar/Rojos/resolve/main/D5K6.0.safetensors?download=true", "name": "D5K6.0.safetensors"},
+    "Merged amateurs - Mixed Amateurs": {"url": "https://civitai.com/api/download/models/179318", "name": "mergedAmateurs_mixedAmateurs.safetensors"},
+    "PornMaster-Pro \u8272\u60c5\u5927\u5e08 - V10.1-VAE-inpainting - V10.1-VAE-inpainting": {"url": "https://civitai.com/api/download/models/937781", "name": "pornmasterProV101VAE_v101VAE-inpainting.safetensors"},
+    "Merged Amateurs - Mixed Amateurs | Inpainting Model - v1.0": {"url": "https://civitai.com/api/download/models/188884", "name": "mergedAmateursMixed_v10-inpainting.safetensors"},
+    "epiCRealism pureEvolution InPainting - v1.0": {"url": "https://civitai.com/api/download/models/95864", "name": "epicrealism_v10-inpainting.safetensors"},
+    "fuego_v2_tkl4_fp26(1)": {"url": "https://huggingface.co/Remphanstar/Rojos/resolve/main/fuego_v2_tkl4_fp26(1).safetensors", "name": "fuego_v2_tkl4_fp26(1).safetensors"},
+    "PornMaster-Pro \u8272\u60c5\u5927\u5e08 - FULL-V4-inpainting - FULL-V4-inpainting": {"url": "https://civitai.com/api/download/models/102709", "name": "pornmasterProFULLV4_fullV4-inpainting.safetensors"},
+    "LazyMix+ (Real Amateur Nudes) - v4.0": {"url": "https://civitai.com/models/10961/lazymix-real-amateur-nudes", "name": "lazymixRealAmateur_v40.safetensors"},
+    "PornMaster-Pro \u8272\u60c5\u5927\u5e08 - FULL-V5-inpainting - FULL-V5-inpainting": {"url": "https://civitai.com/api/download/models/176934", "name": "pornmasterProFULLV5_fullV5-inpainting.safetensors"},
+    "SD.15-AcornMoarMindBreak": {"url": "https://huggingface.co/Remphanstar/Rojos/resolve/main/SD.15-AcornMoarMindBreak.safetensors", "name": "SD.15-AcornMoarMindBreak.safetensors"},
+}
+
+# SD 1.5 VAES
+sd15_vae_data = {
+    "vae-ft-mse-840000-ema-pruned | 840000 | 840k SD1.5 VAE - vae-ft-mse-840k": {"url": "https://civitai.com/api/download/models/311162", "name": "vaeFtMse840000EmaPruned_vaeFtMse840k.safetensors"},
+    "ClearVAE(SD1.5) - v2.3": {"url": "https://civitai.com/api/download/models/88156", "name": "clearvaeSD15_v23.safetensors"},
+}
+
+# SD 1.5 CONTROLNETS
+sd15_controlnet_list = {
+    "1. Canny": [{"url": "https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors"}, {"url": "https://huggingface.co/ckpt/ControlNet-v1-1/raw/main/control_v11p_sd15_canny_fp16.yaml"}],
+}
+
---
+++ scripts/_xl-models-data.py
@@ -0,0 +1,23 @@
+# SDXL MODELS
+sdxl_models_data = {
+    "uberRealisticPornMerge-xlV6Final-inpainting   BEST SO FAR!!! - PonyXL-Hybrid v1": {"url": "https://civitai.com/api/download/models/1024962", "name": "uberrealisticpornmerge_ponyxlHybridV1.safetensors"},
+    "lustifySDXLNSFW_oltINPAINTING": {"url": "https://huggingface.co/RandomGulag/lustifySDXLNSFW_oltINPAINTING/resolve/main/lustifySDXLNSFW_oltINPAINTING.safetensors", "name": "lustifySDXLNSFW_oltINPAINTING.safetensors"},
+    "PornMaster-Pro \u8272\u60c5\u5927\u5e08 - SDXL - SDXL-V2-VAE": {"url": "https://civitai.com/api/download/models/1167499", "name": "pornmasterProSDXL_sdxlV2VAE.safetensors"},
+    "True Amateur Feeling XL - v2": {"url": "https://civitai.com/api/download/models/907265", "name": "trueAmateurFeelingXL_v2.safetensors"},
+    "Sippy": {"url": "https://huggingface.co/Red1618/Zhu/resolve/main/Sippy.safetensors?download=true", "name": "Sippy.safetensors"},
+    "SDXL-DWXL": {"url": "https://huggingface.co/Remphanstar/Rojos/resolve/main/SDXL-DWXL.fp16.safetensors", "name": "SDXL-DWXL.fp16.safetensors"},
+    "PornMaster-\u8272\u60c5\u5927\u5e08 - V7-FP16-VAE": {"url": "https://civitai.com/api/download/models/938228", "name": "pornmaster_v7FP16VAE.safetensors"},
+}
+
+# SDXL VAES
+sdxl_vae_data = {
+    "Pony Standard VAE - V1.0": {"url": "https://civitai.com/api/download/models/785437", "name": "ponyStandardVAE_v10.safetensors"},
+    "FIX FP16 Errors SDXL - Lower Memory use! --- sdxl-vae-fp16-fix by madebyollin - v1.0": {"url": "https://civitai.com/api/download/models/155933", "name": "fixFP16ErrorsSDXLLowerMemoryUse_v10.safetensors"},
+    "SDXL VAE - SDXL-VAE": {"url": "https://civitai.com/api/download/models/333245", "name": "sdxlVAE_sdxlVAE.safetensors"},
+}
+
+# SDXL CONTROLNETS
+sdxl_controlnet_list = {
+    "1. Kohya Controllite XL Blur": [{"url": "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_blur.safetensors"}],
+}
+
